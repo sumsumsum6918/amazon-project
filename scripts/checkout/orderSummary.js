51af98f71cfa0f8a76d7cb11e53ab57c16ba7ec9
sum <tinafongfong@gmail.com>
2024-09-30 16:37:58 +0200

    Move orderSummary into its own file

---
+++ scripts/checkout/orderSummary.js
@@ -0,0 +1,184 @@
+import {
+  cart,
+  removeFromCart,
+  calculateCartQuantity,
+  updateQuantity,
+  updateDeliveryOption,
+  updateCartQuantity,
+  saveQuantity,
+} from "../../data/cart.js";
+import { products } from "../../data/products.js";
+import { formatCurrency } from "../utils/money.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+import { deliveryOptions } from "../../data/deliveryOptions.js";
+
+export function rederOrderSummary() {
+  let cartSummartHTML = "";
+
+  cart.forEach((cartItem) => {
+    const productId = cartItem.productId;
+
+    let matchingProduct;
+
+    products.forEach((product) => {
+      if (product.id === productId) {
+        matchingProduct = product;
+      }
+    });
+
+    const deliveryOptionId = cartItem.deliveryOptionId;
+
+    let deliveryOption;
+
+    deliveryOptions.forEach((option) => {
+      if (option.id === deliveryOptionId) {
+        deliveryOption = option;
+      }
+    });
+
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+
+    const dateString = deliveryDate.format("dddd, MMM D");
+
+    cartSummartHTML += ` 
+     <div class="cart-item-container js-cart-item-container-${
+       matchingProduct.id
+     }">
+          <div class="delivery-date">Delivery date: ${dateString}</div>
+
+            <div class="cart-item-details-grid">
+              <img
+                class="product-image"
+                src="${matchingProduct.image}"
+              />
+
+              <div class="cart-item-details">
+                <div class="product-name">
+                  ${matchingProduct.name}
+                </div>
+                <div class="product-price">$${formatCurrency(
+                  matchingProduct.priceCents
+                )}</div>
+                <div class="product-quantity">
+                  <span> Quantity: <span class="quantity-label js-quantity-label-${
+                    matchingProduct.id
+                  }">${cartItem.quantity}</span> </span>
+                  <span class="update-quantity-link link-primary js-update-link"
+                   data-product-id="${matchingProduct.id}">
+                    Update
+                  </span>
+                  <input type="text" class="quantity-input js-quantity-input js-quantity-input-${
+                    matchingProduct.id
+                  }" data-product-id="${matchingProduct.id}">
+                  <span class="save-quantity-link link-primary js-save-link"  
+                   data-product-id="${matchingProduct.id}">
+                  Save
+                  </span>
+                  <span class="delete-quantity-link link-primary js-delete-link"
+                  data-product-id="${matchingProduct.id}">
+                    Delete
+                  </span>
+                </div>
+              </div>
+
+              <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+                ${deliveryOptionsHTML(matchingProduct, cartItem)}
+              </div>
+            </div>
+          </div>
+  `;
+  });
+
+  function deliveryOptionsHTML(matchingProduct, cartItem) {
+    let html = "";
+
+    deliveryOptions.forEach((deliveryOption) => {
+      const today = dayjs();
+      const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+
+      const dateString = deliveryDate.format("dddd, MMM D");
+
+      const priceString =
+        deliveryOption.priceCents === 0
+          ? "FREE"
+          : `$${formatCurrency(deliveryOption.priceCents)} -`;
+
+      const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+      html += `<div class="delivery-option js-delivery-option"
+                data-product-id="${matchingProduct.id}"
+                data-delivery-option-id="${deliveryOption.id}">
+                  <input
+                    type="radio"
+                    ${isChecked ? "checked" : ""}
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProduct.id}"
+                  />
+                  <div>
+                    <div class="delivery-option-date">${dateString}</div>
+                    <div class="delivery-option-price">${priceString} Shipping</div>
+                  </div>
+                </div>`;
+    });
+
+    return html;
+  }
+
+  document.querySelector(".js-order-summary").innerHTML = cartSummartHTML;
+
+  document.querySelectorAll(".js-delete-link").forEach((link) => {
+    link.addEventListener("click", () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
+      updateCartQuantity();
+    });
+  });
+
+  updateCartQuantity();
+
+  document.querySelectorAll(".js-update-link").forEach((link) => {
+    link.addEventListener("click", () => {
+      const { productId } = link.dataset;
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.add("is-editing-quantity");
+    });
+  });
+
+  document.querySelectorAll(".js-save-link").forEach((link) => {
+    link.addEventListener("click", () => {
+      saveQuantity(link);
+      rederOrderSummary();
+    });
+  });
+
+  document.querySelectorAll(".js-quantity-input").forEach((link) => {
+    link.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        saveQuantity(link);
+      }
+    });
+  });
+
+  document.querySelectorAll(".js-delivery-option").forEach((element) => {
+    element.addEventListener("click", () => {
+      const { productId, deliveryOptionId } = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+      rederOrderSummary();
+    });
+  });
+}
+
+//MVC loops between MVC, it is a design pattern
+//model: saves and manages the data (cart.js)
+//view: takes the data and displays it (checkout.js HTML parts)
+//controller: runs some code when we interact with the page (checkout.js event listeners parts)
