b93a3fba7d3c6a23beef3de75e46506a423cbd01
sum <tinafongfong@gmail.com>
2024-10-30 18:57:57 +0100

    Learn Promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -142,3 +142,14 @@ export function saveQuantity(link) {
 
   updateCartQuantity();
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", () => {
+    console.log(xhr.response);
+
+    fun();
+  });
+  xhr.open("GET", "https://supersimplebackend.dev/cart");
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,11 +2,68 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import "../data/cart-class.js";
 //import "../../Exercises/17/data/car.js";
 //import "../data/backend-practice.js";
-loadProducts(() => {
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve("value2");
+    });
+  }),
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
   renderCheckoutHeader();
 });
+
+/*
+new Promise((resolve) => {
+  //promise runs inner function immediately
+  //resolve control when to go to the next step
+  loadProducts(() => {
+    resolve("value1"); //passable parameter to the next step
+  });
+})
+  .then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+  });
+  */
+
+//Promise create a seperate thread of code
+//function above and below runs at the same time
+//so that JS can do multiplay things at the same time
+//With Promise out codes are more flat
+
+//Promis.all(), let us run multiple promises at the same time
+//,and wait for att of them to finish
+
+//code before making Promise
+// loadProducts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//     renderCheckoutHeader();
+//   });
+// });
+
+//promise does the same thing as a callback
+//callback causes a lot of nesting making code hard to work with
