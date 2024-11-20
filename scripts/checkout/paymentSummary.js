4b26405ffb3e858a0396a65c5c1d806f014a1a9a
sum <tinafongfong@gmail.com>
2024-11-20 14:07:12 +0100

    make place order reset the cart

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,4 +1,4 @@
-import { cart, calculateCartQuantity } from "../../data/cart.js";
+import { cart, calculateCartQuantity, resetCart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
@@ -80,6 +80,8 @@ export function renderPaymentSummary() {
 
         const order = await response.json();
         addOrder(order);
+
+        resetCart();
       } catch (error) {
         console.log("Unexpected error. Try again later.");
       }
