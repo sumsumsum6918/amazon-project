fec37760a186eb987d424d72ec9005d2071cf08b
sumsumsum6918 <tinafongfong@gmail.com>
2024-11-21 11:16:10 +0100

    make progression bar interactive in tracking.js

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -2,6 +2,7 @@ import { updateHeaderQuantity } from "../data/cart.js";
 import { getOrder } from "../data/order.js";
 import { getProduct, loadProductsFetch } from "../data/products.js";
 import { formatDeliveryWeekday } from "../data/deliveryOptions.js";
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
 loadPage();
 async function loadPage() {
@@ -22,7 +23,15 @@ async function loadPage() {
     productDetails.estimatedDeliveryTime
   );
 
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+
+  const percentProgess =
+    ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+
   let orderTrackingHTML = "";
+
   orderTrackingHTML = `
   <a class="back-to-orders-link link-primary" href="orders.html">
           View all orders
@@ -42,13 +51,19 @@ async function loadPage() {
         />
 
         <div class="progress-labels-container">
-          <div class="progress-label">Preparing</div>
-          <div class="progress-label current-status">Shipped</div>
-          <div class="progress-label">Delivered</div>
+          <div class="progress-label ${
+            percentProgess < 50 ? "current-status" : ""
+          }">Preparing</div>
+          <div class="progress-label  ${
+            percentProgess >= 50 && percentProgess < 100 ? "current-status" : ""
+          }">Shipped</div>
+          <div class="progress-label ${
+            percentProgess >= 100 ? "current-status" : ""
+          }">Delivered</div>
         </div>
 
         <div class="progress-bar-container">
-          <div class="progress-bar"></div>
+          <div class="progress-bar" style="width: ${percentProgess}%"></div>
         </div>
   `;
   document.querySelector(".js-order-tracking").innerHTML = orderTrackingHTML;
